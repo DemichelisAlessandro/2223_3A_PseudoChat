63382874c96a5a775d45541cfc95fd60248cdb5a
coding like a pro <mummiamummia735@gmail.com>
2023-03-04 13:29:08 +0100

    verifi

--- js/dati.js
+++ js/dati.js
@@ -4,17 +4,23 @@ var mittenti =[
     1,
     0,
 
+    300,//tappi
+
     0,
     2,
     0,
     2,
     0,
 
+    300,
+
     3,
     0,
     3,
     0,
     3,
+
+    300,
 
     4,
     0,
@@ -23,7 +29,11 @@ var mittenti =[
     0,
     4,
 
-    0
+    300,
+
+    0,
+
+    300
 ];
 
 var destinatari = [
@@ -32,17 +42,23 @@ var destinatari = [
     0,
     1,
 
+    300,
+
     2,
     0,
     2,
     0,
     2,
 
+    300,
+
     0,
     3,
     0,
     3,
     0,
+
+    300,
 
     0,
     4,
@@ -51,7 +67,11 @@ var destinatari = [
     4,
     0,
 
-    5
+    300,
+
+    5,
+
+    300
 ];
 
 var messaggi = [
@@ -60,17 +80,23 @@ var messaggi = [
     "Più o meno, fra poco ho una verifica &#128532;",
     "&#128549;",
 
+    "300",
+
     "Ciao Maria, come è stato il carnevale ad Ivrea?",
     "&#128525; Bellissimo!! &#128151; &#128151;",
     "&#128516; Hai tirato anche tu qualche arancia?",
     "Noooo, sono stata dietro Marco tutto il tempo...",
     "Ah, quindi è venuto anche Marco...",
 
+    "300",
+
     "Ciao Ut!! &#128516; &#128516;",
     "Ehi &#128527;",
     "Sei già arrivato a scuola?",
     "Sì, sono in classe",
     "ohhh &#128519;, come mai non sei passato a salutare?",
+
+    "300",
 
     "Sei arrivato?",
     "Sì, tranquilla",
@@ -79,7 +105,11 @@ var messaggi = [
     "Fra poco vado...",
     "Ma non sei a lezione! &#128545;",
 
-    "Ciao Gigi, oggi a che ora arrivi?"
+    "300",
+
+    "Ciao Gigi, oggi a che ora arrivi?",
+
+    "300"
 ];
 
 var nomeUtenti = [
--- js/script.js
+++ js/script.js
@@ -19,4 +19,135 @@
     - PER IL 10. Gestite il bottone di invio in basso 
         #se il campo di testo non è vuoto aggiungere il messaggio in coda agli altri del personaggio selezionato
 
+                <li>
+                    <div class="material-symbols-outlined icone">
+                        face
+                    </div>
+                    Mario R.
+                </li>
+                <li>
+                    <div class="material-symbols-outlined icone">
+                        face_3
+                    </div>
+                    Maria B.
+                </li>
+
 */
+
+console.log("ciao");
+
+function generafaccie() {
+    let faccie = document.getElementById("listUtenti");
+    let i = 0;
+    let nome;
+    let cognome;
+    let disegno;
+    for (i = 0; i < 5; i++) {
+        nome = nomeUtenti[i];
+        cognome = cognomeUtenti[i];
+        cognome = cognome[0];
+        if (genereUt[i] == "m") {
+            disegno = "face";
+        } else {
+            disegno = "face_3";
+        }
+        if (nomeUtenti[i] != "Ut") {
+            faccie.innerHTML += `
+                 <li>
+                    <div class="material-symbols-outlined icone" onclick="conversazioni("${nome}")">
+                        ${disegno}
+                    </div>
+                    ${nome} ${cognome}.
+                </li>
+`
+        }
+    }
+   
+}
+generafaccie()
+function conversazioni(nome) {
+    let cognome;
+    let nom;
+    let disegno;
+    let chat = document.getElementById("head");
+    let id = nome;
+    for (let i = 0; i < 5; i++) {
+        cognome = cognomeUtenti[i];
+        if (genereUt[i] == "m") {
+            disegno = "face";
+        } else {
+            disegno = "face_3";
+        }
+        nom = nomeUtenti[i];
+        if (nomeUtenti[i] != "Ut") {
+            if (nom == id) {
+                chat.innerHTML = `
+                <div class="material-symbols-outlined icone">
+                    ${disegno}
+                </div>
+                <div>
+                    <div id="divNome">${id} ${cognome}</div>
+                    <div id="divUltimoMes">Oggi alle 9:45</div>
+                </div>
+`
+            }
+        }
+    }
+    chat(id);
+}
+
+function chat(nome) {
+    let id = nome;
+    let ut = 0;
+    let amico;
+    if (id == "Mario") {
+        amico = 1;
+    }
+    if (id == "Maria") {
+        amico = 2;
+    }
+    if (id == "Elena") {
+        amico = 3;
+    }
+    if (id == "Ginevra") {
+        amico = 4;
+    }
+    if (id == "Luigi") {
+        amico = 5;
+    }
+    let sommames = mittenti.length;
+    let inizio;
+    let fine;
+    let ini = false;
+    for (let i = 0; i < sommames; i++) {
+        if (amico == mittenti[i]) {
+            messaggi++;
+            console.log(messaggi[i]);
+            if (mittenti[i - 1] == "300") {
+                inizio = i;
+                ini = true;
+            }
+        }
+        else if (amico != mittenti[i] && mittenti[i] == ut) {
+            messaggi++;
+            console.log(messaggi[i]);
+            if (mittenti[i - 1] == "300") {
+                inizio = i;
+                ini = true;
+            }
+        }
+        else if (mittenti[i] == "300" && ini) {
+            fine = i;
+        }
+    }
+
+    let messaggi = (sommames - inizio) - (sommames - fine);// 0 0 0 0 1 1 1 1 0 0
+    for (let i = 0; i < messaggi; i++) {
+        //stampo i messaggi
+        /*
+         * 
+         * 
+         * 
+         * */
+    }
+}
